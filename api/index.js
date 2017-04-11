e9b8f9e8bd778de30fbd2ca2b246d5ef01993c5a
James Raymond <james@haiku.im>
2017-04-11 12:12:30 +0100

    only show image files, ignore hidden files

--- api/index.js
+++ api/index.js
@@ -5,6 +5,15 @@ const app = module.exports = require('express')();
 
 
 app.get('/:index', (req, res) => {
-  const files = fs.readdirSync(path.resolve(__dirname, '../sample_data/'));
+  let files = fs.readdirSync(path.resolve(__dirname, '../sample_data/'));
+  files = files.filter((file) => {
+    if (file[0] === '.') {
+      return false;
+    }
+    if (file.match(/\.(jpe?g|png|svg|gif|bmp)$/i)) {
+      return true;
+    }
+    return false;
+  })
   res.sendFile(path.resolve(__dirname, '../sample_data', files[req.params.index%files.length]));
 });
